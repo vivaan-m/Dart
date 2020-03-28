c009b8eca0a4724c4de49f24c39d20955e195c97
Vivaan Monga <53035502+vivaan-m@users.noreply.github.com>
2020-03-28 21:29:25 +0530

    Create lecture2.dart

---
+++ lecture2.dart
@@ -0,0 +1,84 @@
+void main(){
+  
+List<int> numbers = [2,3,5,3,4]; // fixed length list 
+List<String> strings = List(); //growable list 
+  
+//   while(num==6){
+//     print("name is random");
+//    // num--;
+//   }
+  
+  
+//   do{
+//     print("i am running");
+//   }while(num==6);
+  
+  //print(numbers.length);
+  //print(numbers[1]);
+//   for(var i = 0;i<numbers.length;i++){
+//     print(numbers[i]);
+//   }
+  
+for(var i in numbers){
+  print(i);
+}
+  
+
+  
+  
+  /*
+  switch(grade){
+    case "A":
+    {
+      print("you have passed your class");
+      
+    }
+    break;
+    case "D":{
+      print("You Failed");
+    }
+      break;
+    default:
+    {
+      print("we don't if you passed or not");
+    }
+      break;
+  }
+  
+  */
+
+  
+  
+//   for(int i=20;i>=0;i--){
+    
+//     if(i%2==0){
+//     }else{
+//     print(i);
+      
+//     }
+    
+//   }
+  
+  
+  /*
+  String name = "uvesh";
+  
+  name=="vivaan"?name="uvesh":name="vivaan";
+  
+  print(value??"there is no value in value");
+  */
+  
+  
+ // exp1??exp2
+  
+  
+  
+  /*
+  if(name=="vivaan"){
+   print("my name is vivaan i am doing dart") ;
+  }else{
+    print("my name is uvesh i am doing dart");
+  }
+  */
+
+}
