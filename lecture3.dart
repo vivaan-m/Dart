9edab18e9430b7d22e4e14f3c4d7c6fa98b640fb
Vivaan Monga <53035502+vivaan-m@users.noreply.github.com>
2020-03-29 20:26:06 +0530

    Create lecture3.dart

---
+++ lecture3.dart
@@ -0,0 +1,87 @@
+void main(){
+  
+  //Lecture 3
+  
+// List numbers = [1,2,3,5];
+// List numbers1 = [1,2,3,5];
+// List numbers2 = [1,2,3,5];
+// List numbers3 = [1,2,3,5];
+// List numbers4 = [1,2,3,5];
+  
+ int user = 0; 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  String name ;
+  //= checkUser(user);
+  
+  
+  checkUser(user);
+  
+  
+  
+  
+  
+  
+  
+  
+  print(checkUser(user));
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //getNumbers(numbers,"vivaan");
+  //getNumbers(numbers1);
+  //getNumbers(numbers2);
+  //getNumbers(numbers3);
+  //getNumbers(numbers4);
+ // getNumbers(string:name,numbers:numbers);
+  //getNumbers();
+}
+String checkUser(user){
+  
+  if(user ==0){
+    
+    return "no users found";
+    
+  }else{
+    return "users found";
+  }
+}
+
+// getNumbers([var numbers,var string]){
+
+// for(var i in numbers){
+//   print(i);
+// }
+ 
+  
+// } 
+// positional c - required + according to position + syntax = none
+
+// optional c - not required + according name + syntax = {}
+
+//postional optional - not required + according to position + syntax = []
+
+
+
+
+
+
+
+//List numbers = [1,2,3,4];
