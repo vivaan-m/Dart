d89e7ec50b8aab6e62029b55087f0b0084f7b697
Vivaan Monga <53035502+vivaan-m@users.noreply.github.com>
2020-04-18 20:24:23 +0530

    Create Lecture8.dart

---
+++ Lecture8.dart
@@ -0,0 +1,35 @@
+//Objectives
+
+//static variables and static metodhs 
+
+void main(){
+
+
+  
+  
+  print(Maths.pie); // 4 btye
+  
+  print(Maths.pie); // 0 byte
+  
+  Maths.games();
+  
+  
+  
+}
+
+
+class Maths{
+  
+  static double pie = 3.14;
+  
+  static void games(){
+    print("games");
+  }
+  
+  
+calculate(){
+  var area = pie; 
+  games();
+ }
+  
+}
